ba841b2f904962ddd51e9bab02373e577267bf53
Ben Allfree <ben@benallfree.com>
2025-04-20 16:37:28 -0700

    Add CORS support for OPTIONS requests

--- packages/site/server/index.ts
+++ packages/site/server/index.ts
@@ -2,6 +2,16 @@
 
 export default {
   async fetch(request, env): Promise<Response> {
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      })
+    }
+
     const url = new URL(request.url)
     if (url.pathname.startsWith('/api/')) {
       // TODO: Add your custom /api/* logic here.
